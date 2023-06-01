35250540313741e584482c77fe704fbeeea1643a
AdriEl Amadi <adrielamadi7@gmail.com>
2023-06-01 03:14:39 +0100

    I built the functionalities to update main admin email, username and I also added email services

--- controllers/mainAdminController/mainAdminAuth.js
+++ controllers/mainAdminController/mainAdminAuth.js
@@ -62,8 +62,6 @@ mainAdminAuth.login = ('/login', async (req, res)=>{
 mainAdminAuth.updatePassword = ('/update-password', async (req, res)=>{
   //get the decoded token
   const decodedToken = req.decodedToken
-  //create token 
-  const newToken = utilities.jwt('sign', {userID: decodedToken.userID, tokenFor: decodedToken.tokenFor})
   let payload = JSON.parse(req.body)
   
   try{
@@ -92,7 +90,7 @@ mainAdminAuth.updatePassword = ('/update-password', async (req, res)=>{
         await database.insertOne({userID: new ObjectId(decodedToken.userID), createdAt: new Date(), otp: newOtp, dataToUpdate: {parameter: 'password', value: payload.newPassword}}, database.collection.pendingUsersUpdates)
   
         //send new otp to email
-        await email.sendOTP('medpharmconsult7@gmail.com', mainAdminObj.email, "OTP Verification", email.OtpMessage(mainAdminObj.username, newOtp))
+        await email.sendOTP('medpharmconsult7@gmail.com', mainAdminObj.email, "OTP Verification", email.OtpMessage(mainAdminObj.username, "password", newOtp))
   
         //send token
         utilities.setResponseData(res, 200, {'content-type': 'application/json'}, {statusCode: 200, msg: "An OTP has been sent to your email"}, true )
@@ -119,172 +117,167 @@ mainAdminAuth.updatePassword = ('/update-password', async (req, res)=>{
 })
 
 
-// adminAuth.verifyUpdateOtp = ('verify-update-otp', async (req, res)=>{
-//   //get the decoded token
-//   const decodedToken = req.decodedToken
-//   //create token 
-//   const newToken = utilities.jwt('sign', {userID: decodedToken.userID, tokenFor: decodedToken.tokenFor})
-//   let payload = JSON.parse(req.body)
+mainAdminAuth.verifyUpdateOtp = ('verify-update-otp', async (req, res)=>{
+  //get the decoded token
+  const decodedToken = req.decodedToken
+  let payload = JSON.parse(req.body)
   
-//   try{
-//     //check if payload is valid
-//     if(utilities.validator(payload, ['otp']).isValid){
-//       //extrract data from the pendingUsersUpdates collection
-//       userObj = await database.findOne({userID: ObjectId(decodedToken.userID)}, database.collection.pendingUsersUpdates, ['otp', 'dataToUpdate'], 1)
+  try{
+    //check if payload is valid
+    if(utilities.otpValidator(payload).isValid){
+      //extract data from the pendingUsersUpdates collection
+      userObj = await database.findOne({userID: new ObjectId(decodedToken.userID)}, database.collection.pendingUsersUpdates, ['otp', 'dataToUpdate'], 1)
    
-//       //check if payload otp matches the otp in the userObj collection
-//       if(payload.otp === userObj.otp){
-//         //update the data of the admin
-//         await database.updateOne({_id: ObjectId(decodedToken.userID)}, database.collection.admins, {[userObj.dataToUpdate.parameter]: userObj.dataToUpdate.value})
+      //check if payload otp matches the otp in the userObj collection
+      if(payload.otp === userObj.otp){
+        //update the data of the main admin
+        await database.updateOne({_id: new ObjectId(decodedToken.userID)}, database.collection.mainAdmins, {[userObj.dataToUpdate.parameter]: userObj.dataToUpdate.value})
   
-//         //delete user from pendingUsersUpdates collection
-//         await database.deleteOne({userID: ObjectId(decodedToken.userID)}, database.collection.pendingUsersUpdates)
+        //delete user from pendingUsersUpdates collection
+        await database.deleteOne({userID: new ObjectId(decodedToken.userID)}, database.collection.pendingUsersUpdates)
   
-//         //send token
-//         utilities.setResponseData(res, 200, {'content-type': 'application/json'}, {statusCode: 200, entamarketToken: newToken}, true )
-//       }
-//       else{
-//         utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {statusCode: 400, msg: `This otp doesn't match the user`, entamarketToken: newToken}, true )
-//         return
-//       }
+        //send token
+        utilities.setResponseData(res, 200, {'content-type': 'application/json'}, {statusCode: 200, msg: "success"}, true )
+      }
+      else{
+        utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {statusCode: 400, msg: `This otp doesn't match the user`}, true )
+        return
+      }
   
-//     }
-//     else{
-//       utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {statusCode: 400, msg: `Invalid data`, entamarketToken: newToken}, true )
-//       return
-//     }
+    }
+    else{
+      utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {statusCode: 400, msg: `Invalid OTP`}, true )
+      return
+    }
      
-//   }
-//   catch(err){
-//     console.log(err)
-//     utilities.setResponseData(res, 500, {'content-type': 'application/json'}, {statusCode: 500, msg: 'Something went wrong with server', entamarketToken: newToken}, true )
-//     return
-//   }
+  }
+  catch(err){
+    console.log(err)
+    utilities.setResponseData(res, 500, {'content-type': 'application/json'}, {statusCode: 500, msg: 'Something went wrong with server'}, true )
+    return
+  }
   
   
-// })
+})
 
 
 
-// adminAuth.updateEmail = ('/update-email', async (req, res)=>{
-//   //get the decoded token
-//   const decodedToken = req.decodedToken
-//   //create token 
-//   const newToken = utilities.jwt('sign', {userID: decodedToken.userID, tokenFor: decodedToken.tokenFor})
-//   let payload = JSON.parse(req.body)
+mainAdminAuth.updateEmail = ('/update-email', async (req, res)=>{
+  //get the decoded token
+  const decodedToken = req.decodedToken
+  let payload = JSON.parse(req.body)
   
-//   try{
-//     //Check if the data sent is valid
-//     if(utilities.validator(payload, ['email', 'password']).isValid){
+  try{
+    //Check if the data sent is valid
+    if(utilities.emailValidator(payload).isValid){
   
-//       //remove all white spaces from user data if any
-//       payload = utilities.trimmer(payload)
+      //remove all white spaces from user data if any
+      payload = utilities.trimmer(payload)
   
-//       //hash the password
-//       payload.password = utilities.dataHasher(payload.password)
+      //hash the password
+      payload.password = utilities.dataHasher(payload.password)
   
-//       //get admin object
-//       const adminObj = await database.findOne({_id: ObjectId(decodedToken.userID)}, database.collection.admins)
+      //get admin object
+      const mainAdminObj = await database.findOne({_id: new ObjectId(decodedToken.userID)}, database.collection.mainAdmins)
   
-//       //check if the payload password matches the password from the trader object
-//       if(payload.password === adminObj.password){
+      //check if the payload password matches the password from the trader object
+      if(payload.password === mainAdminObj.password){
           
-//         //add create otp
-//         const newOtp = utilities.otpMaker()
+        //add create otp
+        const newOtp = utilities.otpMaker()
   
-//         //delete a userID if it exist in the pendingUsersUpdates
-//         await database.deleteOne({userID: ObjectId(decodedToken.userID)}, database.collection.pendingUsersUpdates)
+        //delete a userID if it exist in the pendingUsersUpdates
+        await database.deleteOne({userID: new ObjectId(decodedToken.userID)}, database.collection.pendingUsersUpdates)
   
-//         //add user to pendingUsersUpdates collection
-//         await database.insertOne({userID: ObjectId(decodedToken.userID), createdAt: new Date(), otp: newOtp, dataToUpdate: {parameter: 'email', value: payload.email}}, database.collection.pendingUsersUpdates)
+        //add user to pendingUsersUpdates collection
+        await database.insertOne({userID: new ObjectId(decodedToken.userID), createdAt: new Date(), otp: newOtp, dataToUpdate: {parameter: 'email', value: payload.email}}, database.collection.pendingUsersUpdates)
           
-//         //send the new otp to the new email
-//         await email.sendOtp('entamarketltd@gmail.com', payload.email, "OTP Verification", `hello ${adminObj.username}, please verify your email with this OTP:`, newOtp)
+        //send the new otp to the new email
+        await email.sendOTP('medpharmconsult7@gmail.com', payload.email, "OTP Verification", email.OtpMessage(mainAdminObj.username, "email", newOtp))
   
-//         //send token
-//         utilities.setResponseData(res, 200, {'content-type': 'application/json'}, {statusCode: 200, entamarketToken: newToken}, true )
+        //send token
+        utilities.setResponseData(res, 200, {'content-type': 'application/json'}, {statusCode: 200, msg: "An OTP has been sent to your new email"}, true )
   
   
-//       }
-//       else{
-//         utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {statusCode: 400, msg: `Invalid password`, entamarketToken: newToken}, true )
-//         return
-//       }
+      }
+      else{
+        utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {statusCode: 400, msg: `Invalid password`}, true )
+        return
+      }
   
-//     }
-//     else{
-//       utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {statusCode: 400, msg: `Invalid data`, entamarketToken: newToken}, true )
-//       return
-//     }
+    }
+    else{
+      utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {statusCode: 400, msg: utilities.emailValidator(payload).msg}, true )
+      return
+    }
   
-//   }
-//   catch(err){
-//     console.log(err)
-//     utilities.setResponseData(res, 500, {'content-type': 'application/json'}, {statusCode: 500, msg: 'Something went wrong with server', entamarketToken: newToken}, true )
-//     return
-//   }
+  }
+  catch(err){
+    console.log(err)
+    utilities.setResponseData(res, 500, {'content-type': 'application/json'}, {statusCode: 500, msg: 'Something went wrong with server'}, true )
+    return
+  }
   
-// })
+})
 
 
-// adminAuth.updateUsername = ('/update-username', async (req, res)=>{
-//   //get the decoded token
-//   const decodedToken = req.decodedToken
-//   //create token 
-//   const newToken = utilities.jwt('sign', {userID: decodedToken.userID, tokenFor: decodedToken.tokenFor})
-//   let payload = JSON.parse(req.body)
+mainAdminAuth.updateUsername = ('/update-username', async (req, res)=>{
+  //get the decoded token
+  const decodedToken = req.decodedToken
+  let payload = JSON.parse(req.body)
   
-//   try{
-//     //Check if the data sent is valid
-//     if(utilities.validator(payload, ['username', 'password']).isValid){
+  try{
+    //Check if the data sent is valid
+    if(utilities.usernameValidator(payload).isValid){
   
-//       //remove all white spaces from user data if any
-//       payload = utilities.trimmer(payload)
+      //remove all white spaces from user data if any
+      payload = utilities.trimmer(payload)
   
-//       //hash the password
-//       payload.password = utilities.dataHasher(payload.password)
+      //hash the password
+      payload.password = utilities.dataHasher(payload.password)
   
-//       //get admin object
-//       const adminObj = await database.findOne({_id: ObjectId(decodedToken.userID)}, database.collection.admins)
+      //get admin object
+      const mainAdminObj = await database.findOne({_id: new ObjectId(decodedToken.userID)}, database.collection.mainAdmins)
   
-//       //check if the payload password matches the password from the trader object
-//       if(payload.password === adminObj.password){
+      //check if the payload password matches the password from the trader object
+      if(payload.password === mainAdminObj.password){
           
-//         //add create otp
-//         const newOtp = utilities.otpMaker()
+        //add create otp
+        const newOtp = utilities.otpMaker()
   
-//         //delete a userID if it exist in the pendingUsersUpdates
-//         await database.deleteOne({userID: ObjectId(decodedToken.userID)}, database.collection.pendingUsersUpdates)
+        //delete a userID if it exist in the pendingUsersUpdates
+        await database.deleteOne({userID: new ObjectId(decodedToken.userID)}, database.collection.pendingUsersUpdates)
   
-//         //add user to pendingUsersUpdates collection
-//         await database.insertOne({userID: ObjectId(decodedToken.userID), createdAt: new Date(), otp: newOtp, dataToUpdate: {parameter: 'username', value: payload.username}}, database.collection.pendingUsersUpdates)
+        //add user to pendingUsersUpdates collection
+        await database.insertOne({userID: new ObjectId(decodedToken.userID), createdAt: new Date(), otp: newOtp, dataToUpdate: {parameter: 'username', value: payload.username}}, database.collection.pendingUsersUpdates)
           
-//         //send the new otp to the new email
-//         await email.sendOtp('entamarketltd@gmail.com', adminObj.email, "OTP Verification", `hello ${adminObj.username}, please verify your email with this OTP:`, newOtp)
+        console.log("hello")
+        //send the new otp to the new email
+        await email.sendOTP('medpharmconsult7@gmail.com', mainAdminObj.email, "OTP Verification", email.OtpMessage(mainAdminObj.username, "username", newOtp))
+        console.log("hi")
+        //send token
+        utilities.setResponseData(res, 200, {'content-type': 'application/json'}, {statusCode: 200, msg: "An OTP has been sent to your email"}, true )
   
-//         //send token
-//         utilities.setResponseData(res, 200, {'content-type': 'application/json'}, {statusCode: 200, entamarketToken: newToken}, true )
   
+      }
+      else{
+        utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {statusCode: 400, msg: `Invalid password`}, true )
+        return
+      }
   
-//       }
-//       else{
-//         utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {statusCode: 400, msg: `Invalid password`, entamarketToken: newToken}, true )
-//         return
-//       }
+    }
+    else{
+      utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {statusCode: 400, msg: utilities.usernameValidator(payload).msg}, true )
+      return
+    }
   
-//     }
-//     else{
-//       utilities.setResponseData(res, 400, {'content-type': 'application/json'}, {statusCode: 400, msg: `Invalid data`, entamarketToken: newToken}, true )
-//       return
-//     }
+  }
+  catch(err){
+    console.log(err)
+    utilities.setResponseData(res, 500, {'content-type': 'application/json'}, {statusCode: 500, msg: 'Something went wrong with server'}, true )
+    return
+  }
   
-//   }
-//   catch(err){
-//     console.log(err)
-//     utilities.setResponseData(res, 500, {'content-type': 'application/json'}, {statusCode: 500, msg: 'Something went wrong with server', entamarketToken: newToken}, true )
-//     return
-//   }
-  
-// })
+})
 
 module.exports = mainAdminAuth
--- routes/router.js
+++ routes/router.js
@@ -11,6 +11,8 @@ const {bodyParser, isJwtValid, decodeToken, isJSON} = require('../lib/middleware
 //MAIN ADMIN
 router.get("/mainadmin/login", bodyParser, isJSON, mainAdminAuth.login)
 router.put("/mainadmin/update-password", bodyParser, isJwtValid, decodeToken, isJSON, mainAdminAuth.updatePassword)
-
+router.put("/mainadmin/verify-update-otp", bodyParser, isJwtValid, decodeToken, isJSON, mainAdminAuth.verifyUpdateOtp)
+router.put('/mainadmin/update-email', bodyParser, isJwtValid, decodeToken, isJSON, mainAdminAuth.updateEmail)
+router.put('/mainadmin/update-username', bodyParser, isJwtValid, decodeToken, isJSON, mainAdminAuth.updateUsername)
 
 module.exports = router
